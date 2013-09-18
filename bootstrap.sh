5e6fba60dae18183457a41077784199019101530
Samuel Leathers <sam@appliedtrust.com>
2013-09-17 23:58:04 -0600

    fixing bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/lib/dotfiles" ]; then
-    echo "Installing YADR for the first time"
-    git clone https://github.com/lfilho/dotfiles.git "$HOME/lib/dotfiles"
+    echo "Installing dotfiles for the first time"
+    git clone https://github.com/disassembler/dotfiles.git "$HOME/lib/dotfiles"
     cd "$HOME/lib/dotfiles"
     [ "$1" == "ask" ] && export ASK="true"
     rake install
